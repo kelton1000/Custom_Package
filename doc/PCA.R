16462aabc6766ef223eebbd8457a6bf590703ff3
Kelton <keltonsheridan@gmail.com>
2020-05-19 02:04:30 -0600

    test

---
+++ doc/PCA.R
@@ -0,0 +1,50 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+
+
+## ----setup--------------------------------------------------------------------
+pca <- function(x, retx = TRUE, center = TRUE, scale. = FALSE, tol = NULL, ...)
+  {
+    chkDots(...)
+    x <- as.matrix(x)
+    x <- scale(x, center = center, scale = scale.)
+    cen <- attr(x, "scaled:center")
+    sc <- attr(x, "scaled:scale")
+    if(any(sc == 0))
+      stop("cannot rescale a constant/zero column to unit variance")
+    s <- svd(x, nu = 0)
+    s$d <- s$d / sqrt(max(1, nrow(x) - 1))
+    if (!is.null(tol)) {
+      rank <- sum(s$d > (s$d[1L]*tol))
+      if (rank < ncol(x)) {
+        s$v <- s$v[, 1L:rank, drop = FALSE]
+        s$d <- s$d[1L:rank]
+      }
+    }
+    dimnames(s$v) <-
+      list(colnames(x), paste0("PC", seq_len(ncol(s$v))))
+    r <- list(sdev = s$d, rotation = s$v,
+              center = if(is.null(cen)) FALSE else cen,
+              scale = if(is.null(sc)) FALSE else sc)
+    if (retx) r$x <- x %*% s$v
+    class(r) <- "prcomp"
+    r
+  }
+
+pca_tab <- function(x) {
+  summary(pca(x))}
+
+pca_plot<- function(d, dataframe, groupby, frame = TRUE, frametype){
+  temp<- pca(d)
+  plot<- ggplot2::autoplot(temp, data = dataframe, colour = groupby, frame = frame, frame.type = frametype)
+  return(plot)
+}
+
+## -----------------------------------------------------------------------------
+
+
